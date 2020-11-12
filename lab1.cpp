9af687311d36922a5c2177b9e794bc7119bbfcb4
Artyom <artyomshalev@gmail.com>
2020-11-12 19:30:43 +0300

    format output added

--- lab1.cpp
+++ lab1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>;
 using namespace std;
 
 int main() {
@@ -18,7 +19,13 @@ int main() {
     cin.ignore();
     cin.getline(str, 100);
 
-    cout << "Simple output. Your entered values are:\tinteger -> " << i << "\t float -> " << f << "\t character -> " << c << "\t string -> " << str << endl;
+    cout << "Simple output: Your entered values are:\tinteger -> " << i << "\t float -> " << f << "\t character -> " << c << "\t string -> " << str << endl;
+    cout << "Format output: " << endl
+    << setw(12) << "Variable name " << setw(100) << "Value " << endl
+    << setw(12) << "Integer " << setw(100) << i << endl
+    << setw(12) << "Float " << setw(100) << f << endl
+    << setw(12) << "Character " << setw(100) << c << endl
+    << setw(12) << "String " << setw(100) << str << endl;
 
     cout << "Choose the regime: 1 - write entered values to file; 2 - read previous values from file\n";
     cin >> regime;
